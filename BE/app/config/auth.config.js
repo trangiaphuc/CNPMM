6ac298d4bbd7264d722ec0106bd57f16d67e8680
trangiaphuc <76146929+trangiaphuc@users.noreply.github.com>
2021-10-19 19:28:06 +0700

    init FE, BE of Project and Auth API
    - init two file BE and FE.
    - Create Authentication API
    - Create test Authorization API

---
+++ BE/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "food-and-cook-api-app"
+};
---
+++ BE/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const db = require("./app/models");
+const Role = db.role;
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//sequelize
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+//routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+//initial data in role
+function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
