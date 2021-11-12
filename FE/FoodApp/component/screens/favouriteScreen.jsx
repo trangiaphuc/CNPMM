118b5df023850a017a9d60e9b1de2609f7b4bcde
Nguyễn Văn Huy <nguyenvanhuy21062000@gmail.com>
2021-11-12 17:42:38 +0700

    Update TabNavigate

---
+++ FE/FoodApp/component/screens/favouriteScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View, Text} from "react-native"
+export default function favouriteScreen(){
+    return(
+        <View>
+            <Text>Huy</Text>
+        </View>
+    );
+}
