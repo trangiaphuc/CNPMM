4a6d31f189633a5b331c8df26607471326f732c9
Nguyễn Văn Huy <nguyenvanhuy21062000@gmail.com>
2021-11-30 16:02:53 +0700

    Cart

--- FE/FoodApp/component/screens/cartScreen.jsx
+++ FE/FoodApp/component/screens/cartScreen.jsx
@@ -1,10 +1,45 @@
-import React from "react";
-import {View, Text} from "react-native"
+import React, {useState, useEffect} from "react";
+import {View, Text, FlatList, SafeAreaView} from "react-native"
 import axios from "axios";
-export default function historyScreen(){
+import {Card} from "react-native-elements";
+export default function historyScreen({navigation, route}){
+    const{response}=route.params;
+    const[cart, setCart]=useState([]);
+
+
+    axios.get(`http://192.168.1.33:8080/api/cart/${response.id}`,
+                {
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'x-access-token': response.accessToken,
+                        
+                    },
+                })
+                .then(res => {
+                    setCart(res.data.cart.cartDetails);
+                    
+                    
+                }).catch(error => {
+                        alert('Error', error.res);
+                    
+                });
+
+
+   
+    
+
     return(
-        <View>
-            <Text>Huy</Text>
-        </View>
+        
+        <SafeAreaView>
+            <FlatList
+            data={cart}
+            renderItem={({item})=>
+            <Card>
+                <Text>{item.product.proName}</Text>
+                <Text>{item.quantity}</Text>
+            </Card>
+        }
+            keyExtractor={(item) =>item.id}/>
+        </SafeAreaView>
     );
 }
