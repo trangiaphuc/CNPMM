c506dafed7485cb804578fb13db178ff3bbe352c
Nguyễn Văn Huy <77230707+NguyenVanHuy2106@users.noreply.github.com>
2022-11-24 16:30:02 +0700

    fdfdfdf

--- FE/FoodApp-Merchant/component/services/api.jsx
+++ FE/FoodApp-Merchant/component/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://172.31.99.53:8080/api/`,
+  baseURL: `http://192.168.1.6:8080/api/`,
   //baseURL: `http://172.20.10.8:8080/api/`,
 });
